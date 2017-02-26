e27659c95c6cde00a07907497cd8dfe13e4c42b6
realllittty <realllittty@gmail.com>
2017-02-26 12:34:09 +0200

    realllittty comment 1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,8 @@
 
   #comment bogdan
   #comment2 bogdan
+  #comment reality
+
 
   def day_of_week( wday )
     { 0 => "Неділя", 1 => "Понеділок", 2 => "Вівторок", 3 => "Середа", 4 => "Четвер", 5 => "П'ятниця", 6 => "Субота" }[ wday ]
