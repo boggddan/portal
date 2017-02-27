cb70f5920d318bafdc41654efc0608e1aa2c129c
boggddan <bogdan_uman@meta.ua>
2017-02-27 08:49:00 +0200

    bogdan patch 1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -34,6 +34,9 @@
   #comment9 bogdan
   #comment10 bogdan
 
+  #comment12 reality
+  #comment14 reality
+
 
   def day_of_week( wday )
     { 0 => "Неділя", 1 => "Понеділок", 2 => "Вівторок", 3 => "Середа", 4 => "Четвер", 5 => "П'ятниця", 6 => "Субота" }[ wday ]
