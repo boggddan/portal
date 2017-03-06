e91c064599ea38e5978a0196e2a1c0bfd2c2d83d
boggddan <bogdan_uman@meta.ua>
2017-03-06 11:56:44 +0200

    Megration fix 06-03-2017

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170227124753) do
+ActiveRecord::Schema.define(version: 20170306091133) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "branches", force: :cascade do |t|
-    t.string   "code",       limit: 4
+    t.string   "code",       limit: 11
     t.string   "name",       limit: 50
     t.datetime "created_at",            null: false
     t.datetime "updated_at",            null: false
