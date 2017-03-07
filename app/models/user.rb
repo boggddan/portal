8eb1570b5c8c251d489af22ed0a3e38944743678
boggddan <bogdan_uman@meta.ua>
2017-03-07 10:45:59 +0200

    Fix user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,9 @@
 class User < ApplicationRecord
+  #has_secure_password
 
   belongs_to :userable, polymorphic: true, optional: true
-
-  #belongs_to :institution,  -> { where(userable_type: 'institution') }, foreign_key: :userable_id
-  #belongs_to :supplier, foreign_key: :userable_id
-
-  #has_secure_password
-  #belongs_to :institution
-  #belongs_to :supplier
-
-  #scope :institution, -> { joins( 'institutions' ) }
+  belongs_to :supplier, foreign_key: :userable_id, optional: true
+  belongs_to :institution, foreign_key: :userable_id, optional: true
 
   validates :username, presence: true, uniqueness: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 
 # Первичная настройка пользователя
-User.create( username: 'admin', userable_id: 0, userable_type: 'admin' )
+User.create( username: 'admin', userable_id: 0, userable_type: 'Admin' )
 User.create( username: 'user', userable: Institution.find_by( code: '14' ) )
 
 # Создание для табеля Явки
