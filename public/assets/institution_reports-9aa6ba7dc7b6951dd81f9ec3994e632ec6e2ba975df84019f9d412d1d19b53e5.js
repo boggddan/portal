6e33a5923623104589b12cb9de743d78088ffb87
boggddan <bogdan_uman@meta.ua>
2017-03-20 17:04:48 +0200

    fix style

--- public/assets/institution_reports-9aa6ba7dc7b6951dd81f9ec3994e632ec6e2ba975df84019f9d412d1d19b53e5.js
+++
@@ -1,8 +0,0 @@
-$( document ).on( 'turbolinks:load', function() {
-
-  // Если объект существует
-  if ( $( '#institution_reports' ).length ) {
-    $( '#main_menu li' ).removeClass( 'active' );
-    $( '#mm_institution_reports' ).addClass( 'active' );
-  };
-});
