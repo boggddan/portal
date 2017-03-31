1b529483cc0895b595c5e3d00b7c78804be1aab7
ad <just86@ya.ru>
2017-03-31 14:10:51 +0300

    prefix

--- app/models/institution_order.rb
+++ app/models/institution_order.rb
@@ -14,15 +14,16 @@ class InstitutionOrder < ApplicationRecord
   def set_default_value
     institution = Institution.find( institution_id )
     institution_order = InstitutionOrder.select( :number ).where( institution: institution ).last
-    prefix_length = institution.prefix.strip.length;
+    prefix_length = institution.prefix.strip.length
+    dash = institution_order.number.index('-')
 
     if institution_order
-      number = institution_order.number[prefix_length +1..-1 ].to_i + 1
+      number = institution_order.number[ dash + 1..-1 ].to_i + 1
     else
       number = 1
     end
-
-    self.number ||= "#{ institution.prefix }-#{ number.to_s.rjust(InstitutionOrder.columns_hash["number"].limit-1-prefix_length, '0' ) }"
+    self.number ||= "#{ institution.prefix }-
+      #{ number.to_s.rjust(InstitutionOrder.type_for_attribute("number" ).limit - prefix_length - 1, '0' ) }"
     self.date ||= self.date_start ||=  self.date_end ||= Date.today
   end
 end
