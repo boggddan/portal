9af98d34fadebb0354b54db5f40fded606a1f9c1
boggddan <bogdan_uman@meta.ua>
2017-06-08 23:27:34 +0300

    fix gitignore

--- start.bat
+++
@@ -1,8 +0,0 @@
-@ECHO OFF
-REM SET Env=production
-SET Env=development
-
-TITLE RELEASE -%Env%- [8084]
-
-@ECHO ON
-CD C:\portal_production\ && foreman start --env %Env%.env --procfile Procfile.%Env%
