f8ff17da2b77dfe9bfe12d9e6be17fc62e87cafa
boggddan <bogdan_uman@meta.ua>
2017-10-11 11:22:14 +0300

    db:seeds fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
 # Первичная настройка пользователя
 
-User.create( username: 'admin', password: 'admin', userable_id: 0, userable_type: 'Admin' ) unless User.find_by( username: 'admin' )
-User.create( username: 'user', password: '1', userable: Institution.find_by( code: '14' ) ) unless User.find_by( username: 'user' )
+
+Branch.create( code: '00000000006', name: 'ТВО Вознесенівського району' ) unless User.find_by( code: 14 )
+User.create( code: 14, name: '18 (ДОУ)', branch: Branch.find_by( code: '00000000006' ) ) unless User.find_by( code: 14 )
 
 # Создание для табеля Явки
 ReasonsAbsence.create_with( mark: '', name: 'Явка', priority: 0 ).find_or_create_by( code: '' )
@@ -20,3 +21,6 @@ Dish.create_with( name: 'ЗАГАЛЬНЕ', priority: -1, dishes_category: Dishe
 
 # Пустой тип приема пищи
 Meal.create_with( name: 'ЗАГАЛЬНЕ', priority: 9999 ).find_or_create_by( code: '' )
+
+User.create( username: 'admin', password: 'admin', userable_id: 0, userable_type: 'Admin' ) unless User.find_by( username: 'admin' )
+User.create( username: 'user', password: '1', userable: Institution.find_by( code: '14' ) ) unless User.find_by( username: 'user' )
