75659babe4c8b4c4c19108e70d1372c378addbcb
boggddan <bogdan_uman@meta.ua>
2017-11-16 13:47:45 +0200

    add timesheets errror create

--- app/controllers/institution/timesheets_controller.rb
+++ app/controllers/institution/timesheets_controller.rb
@@ -20,16 +20,21 @@ class Institution::TimesheetsController < Institution::BaseController
     date_start = params[ :date_start ].to_date
     date_end = params[ :date_end ].to_date
 
+    where = <<-SQL.squish
+        ( date_eb BETWEEN '#{ date_start }' AND '#{ date_end }'
+          OR
+          date_ee BETWEEN '#{ date_start }' AND '#{ date_end }' )
+      SQL
+
     timesheet_exists = JSON.parse( Timesheet
-      .select( :id, :number, :date, :date_eb, :date_ee )
+      .select( :id,
+               :number,
+               :date,
+               :date_eb,
+               :date_ee )
       .where( institution_id: current_user[ :userable_id ],
-              date_eb: date_start..date_end,
               is_del_1c: false )
-      .or( Timesheet
-        .select( :id, :number, :date, :date_eb, :date_ee )
-        .where( institution_id: current_user[ :userable_id ],
-                date_ee: date_start..date_end,
-                is_del_1c: false ) )
+      .where( where )
       .order( :number )
       .to_json, symbolize_names: true )
 
@@ -42,9 +47,13 @@ class Institution::TimesheetsController < Institution::BaseController
                      'З': date_str( v[ :date_eb ].to_date ),
                      'ПО': date_str( v[ :date_ee ].to_date ) } } }
     else
-      message = { 'CreateRequest' => { 'StartDate' => date_start,
-                                        'EndDate' => date_end,
-                                        'Institutions_id' => current_institution[ :code ] } }
+      message = {
+        'CreateRequest' => {
+          'StartDate' => date_start,
+          'EndDate' => date_end,
+          'Institutions_id' => current_institution[ :code ]
+        }
+      }
 
       savon_return = get_savon( :get_data_time_sheet, message )
       response = savon_return[ :response ]
@@ -53,125 +62,125 @@ class Institution::TimesheetsController < Institution::BaseController
       result = { }
       ts_data = response[ :ts ]
 
-      ts_data.map { | o | }
-
       if response[ :interface_state ] == 'OK' && ts_data && ts_data.present?
         error = { }
 
-        children_codes = ts_data.group_by { | o | o[ :child_code ] }.map { | k, v | k }
-        children = exists_codes( :children, children_codes )
-        error.merge!( children[ :error ] ) unless children[ :status ]
+        values = ts_data.map.with_index { | o, row |
+          "( #{ row + 1 }#{ row.zero? ? '::INTEGER' : '' },
+            #{ o.values_at( :child_code, :children_group_code, :reasons_absence_code, :date )
+                .map.with_index { | v, i | "'#{ v }'#{ row.zero? ?
+                  if i.between?( 0, 1 ) then '::VARCHAR(9)' elsif i==2 then '::VARCHAR(2)' elsif i==3 then '::DATE' else '' end
+                    : '' }"  }
+          .join( ',' ) } )" }.join( ',' )
 
-        children_groups_codes = ts_data.group_by { | o | o[ :children_group_code ] }.map { | k, v | k }
-        children_groups = exists_codes( :children_groups, children_groups_codes )
-        error.merge!( children_groups[ :error ] ) unless children_groups[ :status ]
+        sql_create_table = <<-SQL.squish
+            ROLLBACK;
+            BEGIN;
 
-        reasons_absences_code = ts_data.group_by { | o | o[ :reasons_absence_code ] }.map { | k, v | k }
-        reasons_absences = exists_codes( :reasons_absences, reasons_absences_code )
-        error.merge!( reasons_absences[ :error ] ) unless reasons_absences[ :status ]
+            CREATE TEMP TABLE timesheets_get (
+                id,
+                child_code,
+                children_group_code,
+                reasons_absence_code,
+                date
+              )
+            ON COMMIT DROP
+            AS ( VALUES #{ values } );
 
-        # values = ts_data.map.with_index { | o, row |
-        #   "( #{ id + 1 }, #{ o.values_at( :child_code, :children_group_code, :reasons_absence_code, :date )
-        #           if i==0 || i==1 then "::VARCHAR(9)" elsif i==2 then "::VARCHAR(2)" elsif i==3 then "::DATE" else "" end
-        #             : "" }"  }
-        #   .join( ',' ) } )" }.join( ',' )
+            CREATE INDEX ON timesheets_get( child_code );
+            CREATE INDEX ON timesheets_get( children_group_code );
+            CREATE INDEX ON timesheets_get( reasons_absence_code );
+          SQL
 
-        # sql_create_table = <<-SQL.squish
-        #     ROLLBACK;
-        #     BEGIN;
+        sql_select = <<-SQL.squish
+          SELECT
+            aa.id,
+            COALESCE( bb.id, 0 ) AS child_id,
+            COALESCE( bb.name, 'Не знайдений код дитини' ) AS child_name,
+            COALESCE( cc.id, 0 ) AS children_group_id,
+            COALESCE( cc.name, 'Не знайдений код групи' ) AS children_group_name,
+            COALESCE( dd.id, 0 ) AS reasons_absence_id,
+            COALESCE( dd.name, 'Не знайденишй код причини відсутності' ) AS reasons_absence_name,
+            aa.child_code,
+            aa.children_group_code,
+            aa.reasons_absence_code,
+            aa.date
+            FROM timesheets_get aa
+            LEFT JOIN children bb ON aa.child_code = bb.code
+            LEFT JOIN children_groups cc ON aa.children_group_code = cc.code
+            LEFT JOIN reasons_absences dd ON aa.reasons_absence_code = dd.code
+            ORDER BY aa.children_group_code,
+                     aa.child_code,
+                     aa.reasons_absence_code,
+                     aa.date;
+        SQL
 
-        #     CREATE TEMP TABLE timesheets_get (
-        #         id,
-        #         child_code,
-        #         children_group_code,
-        #         reasons_absence_code,
-        #         date
-        #       )
-        #     ON COMMIT DROP
-        #     AS ( VALUES #{ values } );
+        timesheets_get = nil
 
-        #     CREATE INDEX ON timesheets_get( child_code );
-        #     CREATE INDEX ON timesheets_get( children_group_code );
-        #     CREATE INDEX ON timesheets_get( reasons_absence_code );
-        #   SQL
+        ActiveRecord::Base.connection_pool
+          .with_connection { | connection |
+            connection.execute( sql_create_table )
+            timesheets_get = JSON.parse( connection.execute( sql_select ).to_json, symbolize_names: true )
+            connection.exec_query( 'COMMIT;' )
+          }
 
-        # sql_select = <<-SQL.squish
-        #   SELECT
-        #     COALESCE( bb.id, 0 ) AS child_id,
-        #     COALESCE( bb.name, 'Не знайдений код дитини' ) AS child_name,
-        #     COALESCE( cc.id, 0 ) AS children_group_id,
-        #     COALESCE( cc.name, 'Не знайдений код групи' ) AS children_group_name,
-        #     COALESCE( dd.id, 0 ) AS reasons_absence_id,
-        #     COALESCE( dd.name, 'Не знайденишй код причини відсутності' ) AS reasons_absence_name,
-        #     aa.child_code,
-        #     aa.children_group_code,
-        #     aa.reasons_absence_code,
-        #     aa.date
-        #     FROM timesheets_get aa
-        #     LEFT JOIN children bb ON aa.child_code = bb.code
-        #     LEFT JOIN children_groups cc ON aa.children_group_code = cc.code
-        #     LEFT JOIN reasons_absences dd ON aa.reasons_absence_code = dd.code
-        #     ORDER BY aa.child_code,
-        #              aa.children_group_code,
-        #              aa.reasons_absence_code,
-        #              aa.date;
-        # SQL
-
-        # timesheets_get = nil
-
-        # ActiveRecord::Base.connection_pool
-        #   .with_connection { | connection |
-        #     connection.execute( sql_create_table )
-        #     timesheets_get = JSON.parse( connection.execute( sql_select ).to_json, symbolize_names: true )
-        #     connection.exec_query( 'COMMIT;' )
-        #   }
-
-        # error = timesheets_get
-        #   .select { | o | o[ :child_id ] || o[ :children_group_id ] || o[ :reasons_absence_id ] }
-        #   .group_by { | o | { o[]}
+        error = timesheets_get
+          .select { | o | o[ :children_group_id ].zero? || o[ :child_id ].zero? || o[ :reasons_absence_id ].zero? }
+          .group_by { | o | [
+            'Код групи': o[ :children_group_code ],
+            'Назва групи': o[ :children_group_name ],
+            'Код дитини': o[ :child_code ],
+            'П.І.Б. дитини': o[ :child_name ],
+            'Код причини відстуності': o[ :reasons_absence_code ],
+            'Причина відсутності': o[ :reasons_absence_name ]
+          ] }.keys
 
         if error.empty?
-          ActiveRecord::Base.transaction do
+          insert_sql_values = timesheets_get.map.with_index { | o, row |
+            "( #{ o.values_at( :children_group_id, :child_id, :reasons_absence_id, :date )
+                  .map.with_index { | v, i | "'#{ v }'#{ row.zero? ?
+                    if i.between?( 0, 2 ) then '::INTEGER' elsif i==3 then '::DATE' else '' end
+                      : '' }"  }
+            .join( ',' ) } )" }.join( ',' )
 
-            data = { institution_id: current_user[ :userable_id ],
-                     branch_id: current_institution[ :branch_id ],
-                     date_vb: response[ :date_vb ],
-                     date_ve: response[ :date_ve ],
-                     date_eb: response[ :date_eb ],
-                     date_ee: response[ :date_ee ] }
+          sql = <<-SQL.squish
+              WITH new_menu_requirement( timesheet_id ) AS (
+                INSERT INTO timesheets
+                  ( institution_id, branch_id, date_vb, date_ve, date_eb, date_ee )
+                  VALUES (
+                    #{ current_user[ :userable_id ] },
+                    #{ current_institution[ :branch_id ] },
+                    '#{ response[ :date_vb ] }',
+                    '#{ response[ :date_ve ] }',
+                    '#{ response[ :date_eb ] }',
+                    '#{ response[ :date_ee ] }' )
+                  RETURNING id
+                  ),
+                  data_td ( children_group_id, child_id, reasons_absence_id, date) AS (
+                    VALUES #{ insert_sql_values } )
+                INSERT INTO timesheet_dates
+                  ( timesheet_id, children_group_id, child_id, reasons_absence_id, date )
+                  SELECT new_menu_requirement.timesheet_id,
+                          data_td.children_group_id,
+                          data_td.child_id,
+                          data_td.reasons_absence_id,
+                          data_td.date
+                    FROM new_menu_requirement,
+                          data_td
+                  RETURNING timesheet_id
+            SQL
 
-            id = insert_base_single( 'timesheets', data )
+          timesheet_id = JSON.parse( ActiveRecord::Base.connection.execute( sql )
+            .to_json, symbolize_names: true )[ 0 ][ :timesheet_id ]
 
-            fields = %w( timesheet_id children_group_id child_id
-                         reasons_absence_id date ).join( ',' )
-
-            sql_values = ''
-
-            ts_data.each { | ts |
-              children_group_id = children_groups[ :obj ][ ( ts[ :children_group_code ] || '' ).strip ]
-              child_id = children[ :obj ][ ( ts[ :child_code ] || '' ).strip ]
-              reasons_absence_id = reasons_absences[ :obj ][ ( ts[ :reasons_absence_code ] || '' ).strip ]
-              sql_values += ",(#{ id }," +
-                            "#{ children_group_id }," +
-                            "#{ child_id }," +
-                            "#{ reasons_absence_id }," +
-                            "'#{ ts[ :date ] }')"
-            }
-
-            sql = "INSERT INTO timesheet_dates ( #{ fields } ) VALUES #{ sql_values[1..-1] }"
-
-            ActiveRecord::Base.connection.execute( sql )
-
-            href = institution_timesheets_dates_path( { id: id } )
-            result = { status: true, href: href }
-          end
+          href = institution_timesheets_dates_path( { id: timesheet_id } )
+          result = { status: true, href: href }
         else
-          result = { status: false, caption: 'Неможливо створити документ',
-                    message: { error: error }.merge!( web_service ) }
+          result = { status: false, caption: 'Неможливо створити документ', message: error }
         end
       else
         result = { status: false, caption: 'За вибраний період даних немає в ІС',
-                  message: web_service.merge!( response: response ) }
+                   message: web_service.merge!( response: response ) }
       end
     end
 
